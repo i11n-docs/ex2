c36c589225b8f2cb9a485e31359e54e4df93910a
Eric L. Bentley <11730553+ebntly@users.noreply.github.com>
2024-12-25 17:44:44 +0000

    added highlighting

--- _config.ts
+++ _config.ts
@@ -1,8 +1,19 @@
 import lume from "lume/mod.ts";
 import wiki from "wiki/mod.ts";
+import prism from "lume/plugins/prism.ts";
+import "npm:prismjs@1.29.0/components/prism-typescript.js";
 
 const site = lume();
+
 site.remoteFile('_includes/main.vto', 'https://raw.githubusercontent.com/lumeland/theme-simple-wiki/refs/heads/main/src/_includes/layout.vto');
+site.use(prism({
+  theme: {
+    name: 'dark',
+    path: '/static/css/prism.css',
+  }
+}));
+
+site.copy('/static')
 site.use(wiki());
 
 export default site;
