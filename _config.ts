71bf8b69c784dc2b027bfb4f1e27c90044a45a6c
Eric L. Bentley <11730553+ebntly@users.noreply.github.com>
2025-08-30 21:45:11 +0000

    docs: migrate to new theme

--- _config.ts
+++ _config.ts
@@ -1,23 +1,8 @@
 import lume from "lume/mod.ts";
-import wiki from "wiki/mod.ts";
-import prism from "lume/plugins/prism.ts";
-import "npm:prismjs@1.29.0/components/prism-typescript.js";
+import theme from "./mod.ts";
 
 const site = lume();
 
-site.remoteFile('_includes/main.vto', 'https://raw.githubusercontent.com/lumeland/theme-simple-wiki/refs/heads/main/src/_includes/layout.vto');
-site.use(prism({
-  theme: {
-    name: 'dark',
-    path: '/static/css/prism.css',
-  }
-}));
-
-site.copy('/static')
-site.use(wiki({
-  favicon: {
-    input: '/favicon.png'
-  }
-}));
+site.use(theme());
 
 export default site;
